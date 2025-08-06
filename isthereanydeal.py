a34cb9f5dd0a6539fc03d9adc1aad55c3dc61d92
Intzider <lukamcm@gmail.com>
2025-08-06 07:22:24 +0200

    fix itad

--- isthereanydeal.py
+++ isthereanydeal.py
@@ -13,12 +13,12 @@ AGENTS = [
 ]
 
 
-def create_new_header(phpsessionid, itad):
+def create_new_header(cookie, itad):
     return {
         'user-agent': "".join([AGENTS[n] + " " for n in sample(range(len(AGENTS)), randint(1, len(AGENTS)))]).strip(),
         "content-type": "application/json",
-        "cookie": f'cookieConsent=1; PHPSESSID={phpsessionid}; country=HR',
-        "itad-sessiontoken": itad,
+        "cookie": f'visitor={cookie.get("visitor")}; sess2={cookie.get("sess2")}; country=HR',
+        "Itad-SessionToken": itad,
     }
 
 
@@ -26,14 +26,13 @@ def get_hits():
     session = requests.Session()
     ses = session.get("https://isthereanydeal.com/giveaways/")
     itad = [json.loads('{' + x + '}').get('token', "") for x in ses.text.split(",") if "token" in x][0]
-    phpsessionid = ses.cookies.get_dict().get("PHPSESSID")
 
-    url = "https://isthereanydeal.com/giveaways/api/list/"
-    data = {"_id": 1, "offset": 0, "sort": None, "filter": None, "options": []}
+    url = "https://isthereanydeal.com/giveaways/api/list/?tab=live"
+    data = { "offset": 0, "filter": None, "sort": None }
 
-    x = requests.post(url, data=json.dumps(data), headers=create_new_header(phpsessionid, itad))
+    x = requests.post(url, data=json.dumps(data), headers=create_new_header(ses.cookies.get_dict(), itad))
 
     hits = [x.get('url', "") for x in json.loads(x.text).get('data', [])]
     hits = [y for y in hits if 'indiegala' not in y]
-    hits = ['<' + y + '>\n' for y in hits]
+    hits = ['<' + (y if "epicgames" not in y else y.split('&')[0]) + '>\n' for y in hits]
     return hits
